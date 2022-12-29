8414c3b0f6308d5b1ea420a84db0ca5807d1345b
mao <1296193245@qq.com>
2022-12-29 14:24:02 +0800

    新增sms-server模块的一些service接口

---
+++ apps/sms/sms-server/src/main/java/mao/sms_server/service/ConfigSignatureService.java
@@ -0,0 +1,22 @@
+package mao.sms_server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import mao.sms_entity.entity.ConfigSignatureEntity;
+
+/**
+ * Project name(项目名称)：sms-backend
+ * Package(包名): mao.sms_server.service
+ * Interface(接口名): ConfigSignatureService
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/12/28
+ * Time(创建时间)： 22:02
+ * Version(版本): 1.0
+ * Description(描述)： 配置—签名
+ */
+
+public interface ConfigSignatureService extends IService<ConfigSignatureEntity>
+{
+
+}
