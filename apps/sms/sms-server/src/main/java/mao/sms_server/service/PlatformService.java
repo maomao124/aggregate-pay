071722289f280ac6f43a664e5d870aaf807c1bae
mao <1296193245@qq.com>
2022-12-30 19:58:12 +0800

    新增sms-server模块的一些service接口

---
+++ apps/sms/sms-server/src/main/java/mao/sms_server/service/PlatformService.java
@@ -0,0 +1,28 @@
+package mao.sms_server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import mao.sms_entity.entity.PlatformEntity;
+
+/**
+ * Project name(项目名称)：sms-backend
+ * Package(包名): mao.sms_server.service
+ * Interface(接口名): PlatformService
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/12/30
+ * Time(创建时间)： 19:56
+ * Version(版本): 1.0
+ * Description(描述)： 接入平台
+ */
+
+public interface PlatformService extends IService<PlatformEntity>
+{
+    /**
+     * 通过名字得到PlatformEntity
+     *
+     * @param name 名字
+     * @return {@link PlatformEntity}
+     */
+    PlatformEntity getByName(String name);
+}
---
+++ apps/sms/sms-server/src/main/java/mao/sms_server/service/ReceiveLogService.java
@@ -0,0 +1,22 @@
+package mao.sms_server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import mao.sms_entity.entity.ReceiveLogEntity;
+
+/**
+ * Project name(项目名称)：sms-backend
+ * Package(包名): mao.sms_server.service
+ * Interface(接口名): ReceiveLogService
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/12/30
+ * Time(创建时间)： 19:57
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public interface ReceiveLogService extends IService<ReceiveLogEntity>
+{
+
+}
