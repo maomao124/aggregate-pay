bd96a8280f9713ca2aa186c7fc72549baefd97ed
mao <1296193245@qq.com>
2022-12-30 22:19:34 +0800

    添加sms-server模块的 黑名单 service实现类

---
+++ apps/sms/sms-server/src/main/java/mao/sms_server/service/impl/BlackListServiceImpl.java
@@ -0,0 +1,24 @@
+package mao.sms_server.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import mao.sms_dao.mapper.BlackListMapper;
+import mao.sms_entity.entity.BlackListEntity;
+import mao.sms_server.service.BlackListService;
+
+/**
+ * Project name(项目名称)：sms-backend
+ * Package(包名): mao.sms_server.service.impl
+ * Class(类名): BlackListServiceImpl
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/12/30
+ * Time(创建时间)： 22:17
+ * Version(版本): 1.0
+ * Description(描述)： 黑名单
+ */
+
+public class BlackListServiceImpl extends ServiceImpl<BlackListMapper, BlackListEntity> implements BlackListService
+{
+
+}
