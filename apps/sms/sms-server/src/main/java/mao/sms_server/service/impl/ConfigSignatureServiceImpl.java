85216e330d1b9b15ff8af70b4292d2e07e992a62
mao <1296193245@qq.com>
2022-12-30 22:20:42 +0800

    添加sms-server模块的 配置—签名 service实现类

--- apps/sms/sms-server/src/main/java/mao/sms_server/service/impl/ConfigSignatureServiceImpl.java
+++ apps/sms/sms-server/src/main/java/mao/sms_server/service/impl/ConfigSignatureServiceImpl.java
@@ -2,27 +2,27 @@ package mao.sms_server.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
-import mao.sms_dao.mapper.BlackListMapper;
-import mao.sms_entity.entity.BlackListEntity;
-import mao.sms_server.service.BlackListService;
+import mao.sms_dao.mapper.ConfigSignatureMapper;
+import mao.sms_entity.entity.ConfigSignatureEntity;
+import mao.sms_server.service.ConfigSignatureService;
 import org.springframework.stereotype.Service;
 
 /**
  * Project name(项目名称)：sms-backend
  * Package(包名): mao.sms_server.service.impl
- * Class(类名): BlackListServiceImpl
+ * Class(类名): ConfigSignatureServiceImpl
  * Author(作者）: mao
  * Author QQ：1296193245
  * GitHub：https://github.com/maomao124/
  * Date(创建日期)： 2022/12/30
- * Time(创建时间)： 22:17
+ * Time(创建时间)： 22:20
  * Version(版本): 1.0
- * Description(描述)： 黑名单
+ * Description(描述)： 配置—签名
  */
 
 @Slf4j
 @Service
-public class BlackListServiceImpl extends ServiceImpl<BlackListMapper, BlackListEntity> implements BlackListService
+public class ConfigSignatureServiceImpl extends ServiceImpl<ConfigSignatureMapper, ConfigSignatureEntity> implements ConfigSignatureService
 {
 
 }
