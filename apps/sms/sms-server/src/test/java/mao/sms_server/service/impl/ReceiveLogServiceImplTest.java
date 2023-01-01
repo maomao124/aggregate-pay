2570dd06c4a0f4c3a157ed336d3ca1434e1ab96e
mao <1296193245@qq.com>
2023-01-01 13:38:47 +0800

    添加sms-server模块的 接收日志 service实现类单元测试

---
+++ apps/sms/sms-server/src/test/java/mao/sms_server/service/impl/ReceiveLogServiceImplTest.java
@@ -0,0 +1,35 @@
+package mao.sms_server.service.impl;
+
+import mao.sms_server.service.ReceiveLogService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Project name(项目名称)：sms-backend
+ * Package(包名): mao.sms_server.service.impl
+ * Class(测试类名): ReceiveLogServiceImplTest
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/1
+ * Time(创建时间)： 13:36
+ * Version(版本): 1.0
+ * Description(描述)： 测试类
+ */
+
+@SpringBootTest
+class ReceiveLogServiceImplTest
+{
+
+    @Autowired
+    private ReceiveLogService receiveLogService;
+
+    @Test
+    void list()
+    {
+        System.out.println(receiveLogService.list());
+    }
+}
