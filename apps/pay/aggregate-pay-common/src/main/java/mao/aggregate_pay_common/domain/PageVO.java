3ad9bbf7a68b3d2118f67d766fdda0c096262471
mao <1296193245@qq.com>
2023-01-07 14:49:11 +0800

    添加聚合支付系统common模块的 PageVO类

---
+++ apps/pay/aggregate-pay-common/src/main/java/mao/aggregate_pay_common/domain/PageVO.java
@@ -0,0 +1,182 @@
+package mao.aggregate_pay_common.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Project name(项目名称)：aggregate-pay
+ * Package(包名): mao.aggregate_pay_common.domain
+ * Class(类名): PageVO
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/7
+ * Time(创建时间)： 14:48
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class PageVO<T> implements Iterable<T>, Serializable
+{
+    /*private List<T> content = new ArrayList<T>();
+        private long total;
+        private int pageNo;
+        private int pageSize;*/
+    private List<T> items = new ArrayList<T>();
+    private long counts;
+    private int page;
+    private int pageSize;
+
+    public PageVO()
+    {
+    }
+
+    public PageVO(List<T> items, long counts, int page, int pageSize)
+    {
+        this.items = items;
+        this.counts = counts;
+        this.page = page;
+        this.pageSize = pageSize;
+    }
+
+    /**
+     * Returns if there is a previous page.
+     *
+     * @return if there is a previous page.
+     */
+    public boolean hasPrevious()
+    {
+        return getPage() > 0;
+    }
+
+    /**
+     * Returns if there is a next page.
+     *
+     * @return if there is a next page.
+     */
+    public boolean hasNext()
+    {
+        return getPage() + 1 < getPages();
+    }
+
+    /**
+     * Returns whether the current page is the first one.
+     *
+     * @return whether the current page is the first one.
+     */
+    public boolean isFirst()
+    {
+        return !hasPrevious();
+    }
+
+    /**
+     * Returns whether the current  page is the last one.
+     *
+     * @return whether the current  page is the last one.
+     */
+    boolean isLast()
+    {
+        return !hasNext();
+    }
+
+    /**
+     * Returns the total amount of elements of all pages.
+     *
+     * @return the total amount of elements of all pages.
+     */
+    public long getCounts()
+    {
+        return counts;
+    }
+
+    public void setCounts(long counts)
+    {
+        this.counts = counts;
+    }
+
+    /**
+     * Returns the number of total pages.
+     *
+     * @return the number of total pages
+     */
+    public int getPages()
+    {
+        return getPageSize() == 0 ? 1 : (int) Math.ceil((double) counts / (double) getPageSize());
+    }
+
+    /**
+     * Returns the page content as unmodifiable {@link List}.
+     *
+     * @return Returns the page content as unmodifiable {@link List}
+     */
+    public List<T> getItems()
+    {
+        return Collections.unmodifiableList(items);
+    }
+
+    public void setItems(List<T> items)
+    {
+        this.items = items;
+    }
+
+    /**
+     * Returns whether the current page has content.
+     *
+     * @return whether the current page has content.
+     */
+    public boolean hasItems()
+    {
+        return getItemsSize() > 0;
+    }
+
+    /**
+     * Returns the number of elements on current page.
+     *
+     * @return the number of elements on current page.
+     */
+    public int getItemsSize()
+    {
+        return items.size();
+    }
+
+    /**
+     * Returns the number of items of each page.
+     *
+     * @return the number of items of each page
+     */
+    public int getPageSize()
+    {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize)
+    {
+        this.pageSize = pageSize;
+    }
+
+    /**
+     * Returns the number of current page. (Zero-based numbering.)
+     *
+     * @return the number of current page.
+     */
+    public int getPage()
+    {
+        return page;
+    }
+
+    /**
+     * Set the number of current page. (Zero-based numbering.)
+     */
+    public void setPage(int page)
+    {
+        this.page = page;
+    }
+
+    public Iterator<T> iterator()
+    {
+        return getItems().iterator();
+    }
+}
