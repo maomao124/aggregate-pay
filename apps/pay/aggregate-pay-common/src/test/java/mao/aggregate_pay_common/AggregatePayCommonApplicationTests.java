0d32f93644cf42d7e6139359ba655a35dcd7923b
mao <1296193245@qq.com>
2023-01-07 14:33:33 +0800

    初始化聚合支付系统common模块

---
+++ apps/pay/aggregate-pay-common/src/test/java/mao/aggregate_pay_common/AggregatePayCommonApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.aggregate_pay_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatePayCommonApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
