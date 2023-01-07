b2232369048de43688aa960c39862b46373af08a
mao <1296193245@qq.com>
2023-01-07 21:24:42 +0800

    添加聚合支付系统common模块的 二维码工具类和单元测试

---
+++ apps/pay/aggregate-pay-common/src/test/java/mao/aggregate_pay_common/utils/QRCodeUtilTest.java
@@ -0,0 +1,39 @@
+package mao.aggregate_pay_common.utils;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Project name(项目名称)：aggregate-pay
+ * Package(包名): mao.aggregate_pay_common.utils
+ * Class(测试类名): QRCodeUtilTest
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/7
+ * Time(创建时间)： 21:21
+ * Version(版本): 1.0
+ * Description(描述)： 测试类
+ */
+
+class QRCodeUtilTest
+{
+
+    @SneakyThrows
+    @Test
+    void createQRCode()
+    {
+        QRCodeUtil qrCodeUtil = new QRCodeUtil();
+        System.out.println(qrCodeUtil.createQRCode("https://fanyi.baidu.com/", 200, 200));
+    }
+
+    @SneakyThrows
+    @Test
+    void createQRCode2()
+    {
+        QRCodeUtil qrCodeUtil = new QRCodeUtil();
+        System.out.println(qrCodeUtil.createQRCode("https://www.bilibili.com/", 200, 200));
+    }
+}
