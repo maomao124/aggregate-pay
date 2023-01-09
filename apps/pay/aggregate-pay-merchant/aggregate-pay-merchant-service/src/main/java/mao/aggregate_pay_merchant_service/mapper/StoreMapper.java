0f66602b64f9778934ce2f031e8d138265b17a7c
mao <1296193245@qq.com>
2023-01-09 14:39:55 +0800

    添加aggregate-pay-merchant-service模块的mapper接口

---
+++ apps/pay/aggregate-pay-merchant/aggregate-pay-merchant-service/src/main/java/mao/aggregate_pay_merchant_service/mapper/StoreMapper.java
@@ -0,0 +1,24 @@
+package mao.aggregate_pay_merchant_service.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import mao.aggregate_pay_merchant_service.entity.Store;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Project name(项目名称)：aggregate-pay
+ * Package(包名): mao.aggregate_pay_merchant_service.mapper
+ * Interface(接口名): StoreMapper
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/9
+ * Time(创建时间)： 14:38
+ * Version(版本): 1.0
+ * Description(描述)： 门店
+ */
+
+@Mapper
+public interface StoreMapper extends BaseMapper<Store>
+{
+
+}
