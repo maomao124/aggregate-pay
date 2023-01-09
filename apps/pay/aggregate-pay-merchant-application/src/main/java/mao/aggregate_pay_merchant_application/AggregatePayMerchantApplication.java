092e0e40eb0ab8d01113891357d99ce8a72ed886
mao <1296193245@qq.com>
2023-01-09 14:27:12 +0800

    修改聚合支付系统pom配置文件和启动类

--- apps/pay/aggregate-pay-merchant-application/src/main/java/mao/aggregate_pay_merchant_application/AggregatePayMerchantApplication.java
+++ apps/pay/aggregate-pay-merchant-application/src/main/java/mao/aggregate_pay_merchant_application/AggregatePayMerchantApplication.java
@@ -1,15 +1,34 @@
 package mao.aggregate_pay_merchant_application;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AggregatePayMerchantApplication
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws UnknownHostException
     {
-        SpringApplication.run(AggregatePayMerchantApplication.class, args);
+        long start = System.currentTimeMillis();
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(AggregatePayMerchantApplication.class, args);
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String appName = environment.getProperty("spring.application.name");
+        String port = environment.getProperty("server.port");
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+
+        //启动完成后在控制台提示项目启动成功，并且输出当前服务对应的swagger接口文档访问地址
+        //http://localhost:8080/doc.html
+        log.info("应用{}启动成功!swagger地址：http://{}:{}/doc.html", appName, hostAddress, port);
+        log.info("启动耗时：" + (System.currentTimeMillis() - start) + "ms");
     }
 
 }
--- apps/pay/aggregate-pay-merchant/aggregate-pay-merchant-service/src/main/java/mao/aggregate_pay_merchant_service/AggregatePayMerchantServiceApplication.java
+++ apps/pay/aggregate-pay-merchant/aggregate-pay-merchant-service/src/main/java/mao/aggregate_pay_merchant_service/AggregatePayMerchantServiceApplication.java
@@ -1,15 +1,33 @@
 package mao.aggregate_pay_merchant_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
 @SpringBootApplication
 public class AggregatePayMerchantServiceApplication
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws UnknownHostException
     {
-        SpringApplication.run(AggregatePayMerchantServiceApplication.class, args);
+        long start = System.currentTimeMillis();
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(AggregatePayMerchantServiceApplication.class, args);
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String appName = environment.getProperty("spring.application.name");
+        String port = environment.getProperty("server.port");
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+
+        //启动完成后在控制台提示项目启动成功，并且输出当前服务对应的swagger接口文档访问地址
+        //http://localhost:8080/doc.html
+        log.info("应用{}启动成功!swagger地址：http://{}:{}/doc.html", appName, hostAddress, port);
+        log.info("数据库监控地址：http://{}:{}/druid",hostAddress, port);
+        log.info("启动耗时：" + (System.currentTimeMillis() - start) + "ms");
     }
 
 }
