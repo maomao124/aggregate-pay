d53eb5418243facd35bd5af8cd68d1bca88908b0
mao <1296193245@qq.com>
2023-01-09 21:28:09 +0800

    初始化模块

---
+++ apps/pay/aggregate-pay-sms/src/main/java/mao/aggregate_pay_sms/AggregatePaySmsApplication.java
@@ -0,0 +1,15 @@
+package mao.aggregate_pay_sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatePaySmsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AggregatePaySmsApplication.class, args);
+    }
+
+}
---
+++ apps/pay/aggregate-pay-sms/src/test/java/mao/aggregate_pay_sms/AggregatePaySmsApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.aggregate_pay_sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatePaySmsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
