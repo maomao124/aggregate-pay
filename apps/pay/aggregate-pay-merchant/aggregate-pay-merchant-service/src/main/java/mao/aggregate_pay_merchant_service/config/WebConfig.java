5cb5830805e707c348dba2cbd51cc69232f65aa5
mao <1296193245@qq.com>
2023-01-09 14:49:08 +0800

    添加aggregate-pay-merchant-service模块的配置类

---
+++ apps/pay/aggregate-pay-merchant/aggregate-pay-merchant-service/src/main/java/mao/aggregate_pay_merchant_service/config/WebConfig.java
@@ -0,0 +1,31 @@
+package mao.aggregate_pay_merchant_service.config;
+
+import lombok.extern.slf4j.Slf4j;
+import mao.tools_common.config.BaseConfig;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Project name(项目名称)：aggregate-pay
+ * Package(包名): mao.aggregate_pay_merchant_service.config
+ * Class(类名): WebConfig
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/9
+ * Time(创建时间)： 14:47
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+@Slf4j
+@Configuration
+public class WebConfig extends BaseConfig
+{
+    @PostConstruct
+    public void init()
+    {
+        log.info("初始化 WebConfig");
+    }
+}
