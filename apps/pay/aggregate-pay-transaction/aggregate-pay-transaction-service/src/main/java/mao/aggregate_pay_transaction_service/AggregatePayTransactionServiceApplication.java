02c0574e617db0548472c243fb8a01ef0a415f7a
mao <1296193245@qq.com>
2023-01-13 14:17:31 +0800

    初始化交易服务

---
+++ apps/pay/aggregate-pay-transaction/aggregate-pay-transaction-service/src/main/java/mao/aggregate_pay_transaction_service/AggregatePayTransactionServiceApplication.java
@@ -0,0 +1,15 @@
+package mao.aggregate_pay_transaction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatePayTransactionServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AggregatePayTransactionServiceApplication.class, args);
+    }
+
+}
---
+++ apps/pay/aggregate-pay-transaction/aggregate-pay-transaction-service/src/test/java/mao/aggregate_pay_transaction_service/AggregatePayTransactionServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.aggregate_pay_transaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatePayTransactionServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
