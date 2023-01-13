169c383145a14bbfa3098fb57d38f4b152029acb
mao <1296193245@qq.com>
2023-01-13 21:31:14 +0800

    修改启动类

--- apps/pay/aggregate-pay-sms/src/main/java/mao/aggregate_pay_sms/AggregatePaySmsApplication.java
+++ apps/pay/aggregate-pay-sms/src/main/java/mao/aggregate_pay_sms/AggregatePaySmsApplication.java
@@ -14,18 +14,31 @@ import java.net.UnknownHostException;
 public class AggregatePaySmsApplication
 {
 
+    /**
+     * 服务启动类
+     *
+     * @param args 参数
+     * @throws UnknownHostException 未知主机异常
+     */
     public static void main(String[] args) throws UnknownHostException
     {
+        //获取开始时间
         long start = System.currentTimeMillis();
+        //启动
         ConfigurableApplicationContext applicationContext = SpringApplication.run(AggregatePaySmsApplication.class, args);
+        //环境
         ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        //获取应用名称
         String appName = environment.getProperty("spring.application.name");
+        //获取端口号
         String port = environment.getProperty("server.port");
+        //获取主机地址
         String hostAddress = InetAddress.getLocalHost().getHostAddress();
 
         //启动完成后在控制台提示项目启动成功，并且输出当前服务对应的swagger接口文档访问地址
         //http://localhost:8080/doc.html
-        log.info("应用{}启动成功!swagger地址：http://{}:{}/doc.html", appName, hostAddress, port);
+        log.info("应用{}启动成功!swagger地址：http://{}:{}/doc.html  或者：http://127.0.0.1:{}/doc.html", appName, hostAddress, port, port);
+        log.info("数据库监控地址：http://{}:{}/druid  或者：http://127.0.0.1:{}/druid", hostAddress, port, port);
         log.info("启动耗时：" + (System.currentTimeMillis() - start) + "ms");
     }
 
