e27ebd2204551ea6a86f9cacd0c5600fd5256c91
mao <1296193245@qq.com>
2023-01-13 22:09:43 +0800

    添加aggregate-pay-transaction-service模块的 Mybatis配置类

---
+++ apps/pay/aggregate-pay-transaction/aggregate-pay-transaction-service/src/main/java/mao/aggregate_pay_transaction_service/config/MybatisConfig.java
@@ -0,0 +1,37 @@
+package mao.aggregate_pay_transaction_service.config;
+
+import lombok.extern.slf4j.Slf4j;
+import mao.tools_databases.datasource.BaseMybatisConfiguration;
+import mao.tools_databases.properties.DatabaseProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Project name(项目名称)：aggregate-pay
+ * Package(包名): mao.aggregate_pay_transaction_service.config
+ * Class(类名): MybatisConfig
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/13
+ * Time(创建时间)： 22:09
+ * Version(版本): 1.0
+ * Description(描述)： Mybatis配置
+ */
+
+@Slf4j
+@Configuration
+public class MybatisConfig extends BaseMybatisConfiguration
+{
+    public MybatisConfig(DatabaseProperties databaseProperties)
+    {
+        super(databaseProperties);
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        log.info("初始化 MybatisConfig");
+    }
+}
