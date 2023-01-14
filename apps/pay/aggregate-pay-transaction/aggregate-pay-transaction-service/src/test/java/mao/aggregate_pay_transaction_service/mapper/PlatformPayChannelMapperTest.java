fec09505ceb27264145777e4be0df6418ca48b03
mao <1296193245@qq.com>
2023-01-14 22:15:13 +0800

    添加aggregate_pay_transaction_service模块的一些mapper单元测试类

---
+++ apps/pay/aggregate-pay-transaction/aggregate-pay-transaction-service/src/test/java/mao/aggregate_pay_transaction_service/mapper/PlatformPayChannelMapperTest.java
@@ -0,0 +1,34 @@
+package mao.aggregate_pay_transaction_service.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Project name(项目名称)：aggregate-pay
+ * Package(包名): mao.aggregate_pay_transaction_service.mapper
+ * Class(测试类名): PlatformPayChannelMapperTest
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/14
+ * Time(创建时间)： 22:13
+ * Version(版本): 1.0
+ * Description(描述)： 测试类
+ */
+
+@SpringBootTest
+class PlatformPayChannelMapperTest
+{
+
+    @Autowired
+    private PlatformPayChannelMapper platformPayChannelMapper;
+
+    @Test
+    void list()
+    {
+        System.out.println(platformPayChannelMapper.selectList(null));
+    }
+}
