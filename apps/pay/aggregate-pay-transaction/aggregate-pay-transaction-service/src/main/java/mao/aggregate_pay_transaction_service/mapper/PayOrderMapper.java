3665afa3d321e6aac1df27e1153f33ea9c633620
mao <1296193245@qq.com>
2023-01-14 22:06:01 +0800

    添加aggregate_pay_transaction_service模块的 PayOrderMapper

---
+++ apps/pay/aggregate-pay-transaction/aggregate-pay-transaction-service/src/main/java/mao/aggregate_pay_transaction_service/mapper/PayOrderMapper.java
@@ -0,0 +1,24 @@
+package mao.aggregate_pay_transaction_service.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import mao.aggregate_pay_transaction_service.entity.PayOrder;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Project name(项目名称)：aggregate-pay
+ * Package(包名): mao.aggregate_pay_transaction_service.mapper
+ * Interface(接口名): PayOrderMapper
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/14
+ * Time(创建时间)： 22:05
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+@Mapper
+public interface PayOrderMapper extends BaseMapper<PayOrder>
+{
+
+}
