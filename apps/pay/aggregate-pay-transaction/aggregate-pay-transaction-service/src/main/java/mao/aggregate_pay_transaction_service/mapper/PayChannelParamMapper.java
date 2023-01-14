d2fa8a1f2e6418e2bb726c685aba53ca2128fb02
mao <1296193245@qq.com>
2023-01-14 22:01:18 +0800

    添加aggregate_pay_transaction_service模块的 PayChannelParamMapper

--- apps/pay/aggregate-pay-transaction/aggregate-pay-transaction-service/src/main/java/mao/aggregate_pay_transaction_service/mapper/PayChannelParamMapper.java
+++ apps/pay/aggregate-pay-transaction/aggregate-pay-transaction-service/src/main/java/mao/aggregate_pay_transaction_service/mapper/PayChannelParamMapper.java
@@ -1,24 +1,24 @@
 package mao.aggregate_pay_transaction_service.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import mao.aggregate_pay_transaction_service.entity.PayChannel;
+import mao.aggregate_pay_transaction_service.entity.PayChannelParam;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
  * Project name(项目名称)：aggregate-pay
  * Package(包名): mao.aggregate_pay_transaction_service.mapper
- * Interface(接口名): PayChannelMapper
+ * Interface(接口名): PayChannelParamMapper
  * Author(作者）: mao
  * Author QQ：1296193245
  * GitHub：https://github.com/maomao124/
  * Date(创建日期)： 2023/1/14
  * Time(创建时间)： 22:00
  * Version(版本): 1.0
- * Description(描述)： 支付聚道
+ * Description(描述)： 某商户针对某一种原始支付渠道的配置参数
  */
 
 @Mapper
-public interface PayChannelMapper extends BaseMapper<PayChannel>
+public interface PayChannelParamMapper extends BaseMapper<PayChannelParam>
 {
 
 }
