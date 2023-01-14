5ca691b584a03e9680bffb15c3c39dfd26a09c27
mao <1296193245@qq.com>
2023-01-14 22:00:27 +0800

    添加aggregate_pay_transaction_service模块的 PayChannelMapper

---
+++ apps/pay/aggregate-pay-transaction/aggregate-pay-transaction-service/src/main/java/mao/aggregate_pay_transaction_service/mapper/PayChannelMapper.java
@@ -0,0 +1,22 @@
+package mao.aggregate_pay_transaction_service.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import mao.aggregate_pay_transaction_service.entity.PayChannel;
+
+/**
+ * Project name(项目名称)：aggregate-pay
+ * Package(包名): mao.aggregate_pay_transaction_service.mapper
+ * Interface(接口名): PayChannelMapper
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/14
+ * Time(创建时间)： 22:00
+ * Version(版本): 1.0
+ * Description(描述)： 支付聚道
+ */
+
+public interface PayChannelMapper extends BaseMapper<PayChannel>
+{
+
+}
