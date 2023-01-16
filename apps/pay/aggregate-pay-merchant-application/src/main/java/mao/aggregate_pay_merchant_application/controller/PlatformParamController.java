33a94ed30860dec5e17e74418b0659e629ddeea0
mao <1296193245@qq.com>
2023-01-16 20:07:17 +0800

    - 商户平台应用支付渠道查询接口

--- apps/pay/aggregate-pay-merchant-application/src/main/java/mao/aggregate_pay_merchant_application/controller/PlatformParamController.java
+++ apps/pay/aggregate-pay-merchant-application/src/main/java/mao/aggregate_pay_merchant_application/controller/PlatformParamController.java
@@ -1,13 +1,18 @@
 package mao.aggregate_pay_merchant_application.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import mao.aggregate_pay_merchant_application.handler.AssertResult;
+import mao.aggregate_pay_transaction_api.dto.PayChannelDTO;
 import mao.aggregate_pay_transaction_api.dto.PlatformChannelDTO;
+import mao.aggregate_pay_transaction_api.feign.PayChannelFeignClient;
 import mao.aggregate_pay_transaction_api.feign.PlatformChannelFeignClient;
 import mao.tools_core.base.R;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -34,6 +39,15 @@ public class PlatformParamController
     @Resource
     private PlatformChannelFeignClient platformChannelFeignClient;
 
+    @Resource
+    private PayChannelFeignClient payChannelFeignClient;
+
+
+    /**
+     * 获取平台所有服务类型
+     *
+     * @return {@link List}<{@link PlatformChannelDTO}>
+     */
     @ApiOperation("获取平台服务类型")
     @GetMapping(value = "/my/platform‐channels")
     public List<PlatformChannelDTO> queryPlatformChannel()
@@ -45,5 +59,29 @@ public class PlatformParamController
         //返回
         return r.getData();
     }
+
+    /**
+     * 根据平台服务类型获取支付渠道列表
+     *
+     * @param platformChannelCode 服务类型编码
+     * @return {@link List}<{@link PayChannelDTO}>
+     */
+    @ApiOperation("根据平台服务类型获取支付渠道列表")
+    @ApiImplicitParams(
+            {
+                    @ApiImplicitParam(name = "platformChannelCode", value = "服务类型编码", required =
+                            true, dataType = "String", paramType = "path")
+            })
+    @GetMapping(value = "/my/pay‐channels/platform‐channel/{platformChannelCode}")
+    public List<PayChannelDTO> queryPayChannelByPlatformChannel(@PathVariable String platformChannelCode)
+    {
+        //远程调用
+        R<List<PayChannelDTO>> r = payChannelFeignClient.queryPayChannelByPlatformChannel(platformChannelCode);
+        //断言
+        AssertResult.handler(r);
+        //返回
+        return r.getData();
+    }
+
 
 }
