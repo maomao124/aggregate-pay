e5014b2925c337675fb9e69a33003a24f208365e
mao <1296193245@qq.com>
2023-01-17 15:09:42 +0800

    初始化模块

---
+++ apps/pay/aggregate-pay-gateway/src/test/java/mao/aggregate_pay_gateway/AggregatePayGatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.aggregate_pay_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatePayGatewayApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
