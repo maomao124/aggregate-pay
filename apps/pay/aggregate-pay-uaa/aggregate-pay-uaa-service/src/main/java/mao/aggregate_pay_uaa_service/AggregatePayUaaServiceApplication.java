c110359c51f8a60a44362cf2b7204b3e7182c9cd
mao <1296193245@qq.com>
2023-01-17 15:15:06 +0800

    初始化认证和授权模块

---
+++ apps/pay/aggregate-pay-uaa/aggregate-pay-uaa-service/src/main/java/mao/aggregate_pay_uaa_service/AggregatePayUaaServiceApplication.java
@@ -0,0 +1,15 @@
+package mao.aggregate_pay_uaa_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatePayUaaServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AggregatePayUaaServiceApplication.class, args);
+    }
+
+}
