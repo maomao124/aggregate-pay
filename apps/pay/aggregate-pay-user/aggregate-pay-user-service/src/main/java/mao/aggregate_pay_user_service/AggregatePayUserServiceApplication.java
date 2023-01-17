c0f945221100cdce37c3b22b2d7369af38c18f3c
mao <1296193245@qq.com>
2023-01-17 16:55:47 +0800

    初始化用户模块

---
+++ apps/pay/aggregate-pay-user/aggregate-pay-user-service/src/main/java/mao/aggregate_pay_user_service/AggregatePayUserServiceApplication.java
@@ -0,0 +1,15 @@
+package mao.aggregate_pay_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatePayUserServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AggregatePayUserServiceApplication.class, args);
+    }
+
+}
---
+++ apps/pay/aggregate-pay-user/aggregate-pay-user-service/src/test/java/mao/aggregate_pay_user_service/AggregatePayUserServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.aggregate_pay_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatePayUserServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
