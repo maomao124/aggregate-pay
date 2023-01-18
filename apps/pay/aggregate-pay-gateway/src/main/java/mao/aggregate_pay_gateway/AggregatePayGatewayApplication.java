e412900de343f2320d83dfa496fc46d0babc6d6b
mao <1296193245@qq.com>
2023-01-18 13:38:22 +0800

    修改启动类

--- apps/pay/aggregate-pay-gateway/src/main/java/mao/aggregate_pay_gateway/AggregatePayGatewayApplication.java
+++ apps/pay/aggregate-pay-gateway/src/main/java/mao/aggregate_pay_gateway/AggregatePayGatewayApplication.java
@@ -1,15 +1,47 @@
 package mao.aggregate_pay_gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AggregatePayGatewayApplication
 {
-
-    public static void main(String[] args)
+    /**
+     * 服务启动类
+     *
+     * @param args 参数
+     * @throws UnknownHostException 未知主机异常
+     */
+    public static void main(String[] args) throws UnknownHostException
     {
-        SpringApplication.run(AggregatePayGatewayApplication.class, args);
+        //获取开始时间
+        long start = System.currentTimeMillis();
+        //启动
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(AggregatePayGatewayApplication.class, args);
+        //环境
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        //获取应用名称
+        String appName = environment.getProperty("spring.application.name");
+        //获取端口号
+        String port = environment.getProperty("server.port");
+        //获取主机地址
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+
+        //启动完成后在控制台提示项目启动成功，并且输出当前服务对应的swagger接口文档访问地址
+        //http://localhost:8080/doc.html
+        log.info("应用{}启动成功!", appName);
+        log.info("启动耗时：" + (System.currentTimeMillis() - start) + "ms");
     }
 
 }
--- apps/pay/aggregate-pay-uaa/aggregate-pay-uaa-service/src/main/java/mao/aggregate_pay_uaa_service/AggregatePayUaaServiceApplication.java
+++ apps/pay/aggregate-pay-uaa/aggregate-pay-uaa-service/src/main/java/mao/aggregate_pay_uaa_service/AggregatePayUaaServiceApplication.java
@@ -1,15 +1,47 @@
 package mao.aggregate_pay_uaa_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AggregatePayUaaServiceApplication
 {
 
-    public static void main(String[] args)
+    /**
+     * 服务启动类
+     *
+     * @param args 参数
+     * @throws UnknownHostException 未知主机异常
+     */
+    public static void main(String[] args) throws UnknownHostException
     {
-        SpringApplication.run(AggregatePayUaaServiceApplication.class, args);
+        //获取开始时间
+        long start = System.currentTimeMillis();
+        //启动
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(AggregatePayUaaServiceApplication.class, args);
+        //环境
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        //获取应用名称
+        String appName = environment.getProperty("spring.application.name");
+        //获取端口号
+        String port = environment.getProperty("server.port");
+        //获取主机地址
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+
+        //启动完成后在控制台提示项目启动成功，并且输出当前服务对应的swagger接口文档访问地址
+        //http://localhost:8080/doc.html
+        log.info("应用{}启动成功!swagger地址：http://{}:{}/doc.html  或者：http://127.0.0.1:{}/doc.html", appName, hostAddress, port, port);
+        log.info("数据库监控地址：http://{}:{}/druid  或者：http://127.0.0.1:{}/druid", hostAddress, port, port);
+        log.info("启动耗时：" + (System.currentTimeMillis() - start) + "ms");
     }
 
 }
