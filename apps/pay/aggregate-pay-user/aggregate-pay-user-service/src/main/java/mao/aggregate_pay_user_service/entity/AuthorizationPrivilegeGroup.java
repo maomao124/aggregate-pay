0258de254e05da5048cfaf667f38eb7173b0e8eb
mao <1296193245@qq.com>
2023-01-18 20:35:18 +0800

    添加aggregate_pay_user_service模块一些实体类

---
+++ apps/pay/aggregate-pay-user/aggregate-pay-user-service/src/main/java/mao/aggregate_pay_user_service/entity/AuthorizationPrivilegeGroup.java
@@ -0,0 +1,52 @@
+package mao.aggregate_pay_user_service.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * Project name(项目名称)：aggregate-pay
+ * Package(包名): mao.aggregate_pay_user_service.entity
+ * Class(类名): AuthorizationPrivilegeGroup
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/18
+ * Time(创建时间)： 20:34
+ * Version(版本): 1.0
+ * Description(描述)： 权限组
+ */
+
+@Data
+@TableName("authorization_privilege_group")
+public class AuthorizationPrivilegeGroup  implements Serializable
+{
+
+    /**
+     * 串行版本uid
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    @TableId(value="ID",type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 父id
+     */
+    @TableField("PARENT_ID")
+    private Long parentId;
+
+    /**
+     * 权限组名称
+     */
+    @TableField("NAME")
+    private String name;
+
+}
