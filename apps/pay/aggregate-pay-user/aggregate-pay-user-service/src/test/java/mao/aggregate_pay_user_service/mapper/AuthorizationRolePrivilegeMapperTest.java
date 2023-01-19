6ed6973a7472102e8aeaa4fb3fbf471c8918adfd
mao <1296193245@qq.com>
2023-01-19 18:22:27 +0800

    添加aggregate_pay_user_service模块 AuthorizationRolePrivilegeMapper 单元测试

---
+++ apps/pay/aggregate-pay-user/aggregate-pay-user-service/src/test/java/mao/aggregate_pay_user_service/mapper/AuthorizationRolePrivilegeMapperTest.java
@@ -0,0 +1,51 @@
+package mao.aggregate_pay_user_service.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Project name(项目名称)：aggregate-pay
+ * Package(包名): mao.aggregate_pay_user_service.mapper
+ * Class(测试类名): AuthorizationRolePrivilegeMapperTest
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/19
+ * Time(创建时间)： 18:17
+ * Version(版本): 1.0
+ * Description(描述)： 测试类
+ */
+
+
+@Transactional
+@SpringBootTest
+class AuthorizationRolePrivilegeMapperTest
+{
+    @Autowired
+    private AuthorizationRolePrivilegeMapper authorizationRolePrivilegeMapper;
+
+    /**
+     * List.
+     */
+    @Test
+    void list()
+    {
+        System.out.println(authorizationRolePrivilegeMapper.selectList(null));
+    }
+
+    /**
+     * Insert role privilege.
+     */
+    @Test
+    void insertRolePrivilege()
+    {
+        authorizationRolePrivilegeMapper.insertRolePrivilege(133333L, Collections.singletonList(11111L));
+    }
+}
