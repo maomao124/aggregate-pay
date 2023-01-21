a9899ccbf4b8b102235391b255b4bfe13d90e823
mao <1296193245@qq.com>
2023-01-21 19:58:14 +0800

    修改aggregate_pay_uaa_service模块 启动类

--- apps/pay/aggregate-pay-uaa/aggregate-pay-uaa-service/src/main/java/mao/aggregate_pay_uaa_service/AggregatePayUaaServiceApplication.java
+++ apps/pay/aggregate-pay-uaa/aggregate-pay-uaa-service/src/main/java/mao/aggregate_pay_uaa_service/AggregatePayUaaServiceApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
 
@@ -13,6 +14,7 @@ import java.net.UnknownHostException;
 @Slf4j
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients(value = {"mao"})
 public class AggregatePayUaaServiceApplication
 {
 
@@ -39,8 +41,8 @@ public class AggregatePayUaaServiceApplication
 
         //启动完成后在控制台提示项目启动成功，并且输出当前服务对应的swagger接口文档访问地址
         //http://localhost:8080/doc.html
-        log.info("应用{}启动成功!swagger地址：http://{}:{}/doc.html  或者：http://127.0.0.1:{}/doc.html", appName, hostAddress, port, port);
-        log.info("数据库监控地址：http://{}:{}/druid  或者：http://127.0.0.1:{}/druid", hostAddress, port, port);
+        log.info("应用{}启动成功!swagger地址：http://{}:{}/doc.html  或者：http://127.0.0.1:{}/uaa/doc.html", appName, hostAddress, port, port);
+        log.info("数据库监控地址：http://{}:{}/druid  或者：http://127.0.0.1:{}/uaa/druid", hostAddress, port, port);
         log.info("启动耗时：" + (System.currentTimeMillis() - start) + "ms");
     }
 
