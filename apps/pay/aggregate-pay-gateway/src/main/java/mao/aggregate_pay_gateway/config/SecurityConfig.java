4c5ea0f25736f7ec3a7732e8db7a280feb1bdcd3
mao <1296193245@qq.com>
2023-01-22 22:33:15 +0800

    添加aggregate_pay_gateway模块 SecurityConfig

---
+++ apps/pay/aggregate-pay-gateway/src/main/java/mao/aggregate_pay_gateway/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package mao.aggregate_pay_gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Project name(项目名称)：aggregate-pay
+ * Package(包名): mao.aggregate_pay_gateway.config
+ * Class(类名): SecurityConfig
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/1/22
+ * Time(创建时间)： 22:31
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and().csrf().disable();
+
+    }
+}
