b4a3de8091ff3980f732118c57eea5e7e80ece45
mao <1296193245@qq.com>
2023-01-27 21:22:18 +0800

    修改交易服务AliPayConfigurationProperties

--- apps/pay/aggregate-pay-transaction/aggregate-pay-transaction-service/src/main/java/mao/aggregate_pay_transaction_service/properties/AliPayConfigurationProperties.java
+++ apps/pay/aggregate-pay-transaction/aggregate-pay-transaction-service/src/main/java/mao/aggregate_pay_transaction_service/properties/AliPayConfigurationProperties.java
@@ -21,6 +21,22 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "alipay")
 public class AliPayConfigurationProperties
 {
+
+    /**
+     * 编码
+     */
+    private String charSet = "UTF-8";
+
+    /**
+     * 服务网关地址
+     */
+    private String serverUrl = "https://openapi.alipaydev.com/gateway.do";
+
+    /**
+     * 加密方法
+     */
+    private String encryptionMethod = "RSA2";
+
     /**
      * 应用程序id
      */
@@ -48,15 +64,81 @@ public class AliPayConfigurationProperties
     /**
      * 阿里支付配置属性
      *
-     * @param appId           应用程序id
-     * @param appPrivateKey   应用私钥
-     * @param alipayPublicKey 支付宝公钥
+     * @param charSet          字符组
+     * @param serverUrl        服务器url
+     * @param encryptionMethod 加密方法
+     * @param appId            应用程序id
+     * @param appPrivateKey    应用私钥
+     * @param alipayPublicKey  支付宝公钥
      */
-    public AliPayConfigurationProperties(String appId, String appPrivateKey, String alipayPublicKey)
+    public AliPayConfigurationProperties(String charSet, String serverUrl, String encryptionMethod, String appId, String appPrivateKey, String alipayPublicKey)
     {
+        this.charSet = charSet;
+        this.serverUrl = serverUrl;
+        this.encryptionMethod = encryptionMethod;
         this.appId = appId;
         this.appPrivateKey = appPrivateKey;
         this.alipayPublicKey = alipayPublicKey;
+    }
+
+    /**
+     * 获取字符设置
+     *
+     * @return {@link String}
+     */
+    public String getCharSet()
+    {
+        return charSet;
+    }
+
+    /**
+     * 字符集合
+     *
+     * @param charSet 字符组
+     */
+    public void setCharSet(String charSet)
+    {
+        this.charSet = charSet;
+    }
+
+    /**
+     * 获取服务器url
+     *
+     * @return {@link String}
+     */
+    public String getServerUrl()
+    {
+        return serverUrl;
+    }
+
+    /**
+     * 设置服务器url
+     *
+     * @param serverUrl 服务器url
+     */
+    public void setServerUrl(String serverUrl)
+    {
+        this.serverUrl = serverUrl;
+    }
+
+    /**
+     * 得到加密方法
+     *
+     * @return {@link String}
+     */
+    public String getEncryptionMethod()
+    {
+        return encryptionMethod;
+    }
+
+    /**
+     * 设置加密方法
+     *
+     * @param encryptionMethod 加密方法
+     */
+    public void setEncryptionMethod(String encryptionMethod)
+    {
+        this.encryptionMethod = encryptionMethod;
     }
 
     /**
