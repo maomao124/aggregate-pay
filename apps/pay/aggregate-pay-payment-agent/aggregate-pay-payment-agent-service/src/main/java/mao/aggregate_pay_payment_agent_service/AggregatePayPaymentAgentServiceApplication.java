644c85f6fe6460bb960795349e7b887922292300
mao <1296193245@qq.com>
2023-01-30 21:41:42 +0800

    初始化支付渠道代理模块

---
+++ apps/pay/aggregate-pay-payment-agent/aggregate-pay-payment-agent-service/src/main/java/mao/aggregate_pay_payment_agent_service/AggregatePayPaymentAgentServiceApplication.java
@@ -0,0 +1,15 @@
+package mao.aggregate_pay_payment_agent_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregatePayPaymentAgentServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AggregatePayPaymentAgentServiceApplication.class, args);
+    }
+
+}
---
+++ apps/pay/aggregate-pay-payment-agent/aggregate-pay-payment-agent-service/src/test/java/mao/aggregate_pay_payment_agent_service/AggregatePayPaymentAgentServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.aggregate_pay_payment_agent_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatePayPaymentAgentServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
